0bc03a5258243bbde3a7b19c155a79e4e412d496
Adrian Luque Bueso <al387532@uji.es>
2021-04-06 22:35:33 +0200

    +9daos

---
+++ src/main/java/dao/ReservationZoneRowMapper.java
@@ -0,0 +1,18 @@
+package dao;
+
+import model.ReservationZone;
+import org.springframework.jdbc.core.RowMapper;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class ReservationZoneRowMapper implements RowMapper<ReservationZone> {
+    @Override
+    public ReservationZone mapRow(ResultSet resultSet, int i) throws SQLException {
+        ReservationZone reservationZone = new ReservationZone();
+        reservationZone.setId(resultSet.getInt("id"));
+        reservationZone.setReservation(resultSet.getInt("reservation"));
+        reservationZone.setZone(resultSet.getInt("zone"));
+        return reservationZone;
+    }
+}
