850c0c76ee04847e365495204fa9b986c0665d85
Adrian Luque Bueso <al387532@uji.es>
2021-04-06 19:46:47 +0200

    Zone fixed + place

---
+++ src/main/java/model/Place.java
@@ -0,0 +1,45 @@
+package model;
+
+public class Place {
+    private int id;
+    private String name;
+    private String location;
+
+    public Place(){
+
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    @Override
+    public String toString() {
+        return "Place{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", location='" + location + '\'' +
+                '}';
+    }
+
+}
---
+++ src/main/java/model/Zone.java
@@ -0,0 +1,26 @@
+package model;
+
+import java.time.LocalDateTime;
+
+public class Zone {
+    private int place;
+
+    public int getPlace() {
+        return place;
+    }
+
+    public void setPlace(int place) {
+        this.place = place;
+    }
+
+    public Zone(){
+
+    }
+
+    @Override
+    public String toString() {
+        return "Zone{" +
+                "place=" + place +
+                '}';
+    }
+}
