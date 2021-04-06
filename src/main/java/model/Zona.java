0e1b0f679ba48d0c5793d7c7eb02a8903d38bfc9
Adrian Luque Bueso <al387532@uji.es>
2021-04-06 19:41:10 +0200

    Zona++++

--- src/main/java/model/Zona.java
+++ src/main/java/model/Zona.java
@@ -1,4 +1,55 @@
 package model;
 
+import java.time.LocalDateTime;
+
 public class Zona {
+    private String location;
+    private int maximumCapacity;
+    private LocalDateTime signUpDate;
+    private LocalDateTime signDownDate;
+
+    public Zona() {
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public int getMaximumCapacity() {
+        return maximumCapacity;
+    }
+
+    public LocalDateTime getSignUpDate() {
+        return signUpDate;
+    }
+
+    public LocalDateTime getSignDownDate() {
+        return signDownDate;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public void setMaximumCapacity(int maximumCapacity) {
+        this.maximumCapacity = maximumCapacity;
+    }
+
+    public void setSignUpDate(LocalDateTime signUpDate) {
+        this.signUpDate = signUpDate;
+    }
+
+    public void setSignDownDate(LocalDateTime signDownDate) {
+        this.signDownDate = signDownDate;
+    }
+
+    @Override
+    public String toString() {
+        return "Zona{" +
+                "location='" + location + '\'' +
+                ", maximumCapacity=" + maximumCapacity +
+                ", signUpDate=" + signUpDate +
+                ", signDownDate=" + signDownDate +
+                '}';
+    }
 }
