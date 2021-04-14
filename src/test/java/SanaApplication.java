bd5f3ea5ccf7645a11ceff88ddd770da1a7e5aa4
Miguel Ángel Prosper <37023010+MAProsper@users.noreply.github.com>
2021-04-14 19:16:35 +0200

    stuff

--- src/test/java/SanaApplication.java
+++ src/test/java/SanaApplication.java
@@ -3,23 +3,15 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.jdbc.core.JdbcTemplate;
 
-import javax.sql.DataSource;
 import java.util.logging.Logger;
 
 @SpringBootApplication
 public class SanaApplication implements CommandLineRunner {
-    private JdbcTemplate jdbcTemplate;
     private static final Logger log = Logger.getLogger(SanaApplication.class.getName());
 
     @Autowired
     AddressDao addressDao;
-
-    @Autowired
-    public void setDataSource(DataSource dataSource) {
-        jdbcTemplate = new JdbcTemplate(dataSource);
-    }
 
     public static void main(String[] args) {
         new SpringApplicationBuilder(SanaApplication.class).run(args);
