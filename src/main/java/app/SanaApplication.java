0b1fff0954777878161cc8bbf2fb03e2e81fd14e
Miguel Ángel Prosper <37023010+MAProsper@users.noreply.github.com>
2021-04-14 22:09:48 +0200

    stuff

--- src/main/java/app/SanaApplication.java
+++ src/main/java/app/SanaApplication.java
@@ -1,3 +1,7 @@
+package app;
+
+import dao.address.AddressDao;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
@@ -8,10 +12,19 @@ import java.util.logging.Logger;
 public class SanaApplication implements CommandLineRunner {
     private static final Logger log = Logger.getLogger(SanaApplication.class.getName());
 
+    @Autowired
+    AddressDao addressDao;
+
     public static void main(String[] args) {
         new SpringApplicationBuilder(SanaApplication.class).run(args);
     }
 
     public void run(String... strings) {
+        testDao();
+    }
+
+    void testDao() {
+        log.info("test/dao");
+        log.info(addressDao.getAll().toString());
     }
 }
