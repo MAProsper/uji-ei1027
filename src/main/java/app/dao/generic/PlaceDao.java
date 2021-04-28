3969b813bbb96b7b8f4c670839a7e4f2110cb803
Miguel Ángel Prosper <37023010+MAProsper@users.noreply.github.com>
2021-04-28 19:52:54 +0200

    stuff

--- src/main/java/app/dao/generic/PlaceDao.java
+++ src/main/java/app/dao/generic/PlaceDao.java
@@ -1,8 +1,6 @@
 package app.dao.generic;
 
 import app.model.generic.Place;
-import org.springframework.stereotype.Repository;
 
-@Repository
 public abstract class PlaceDao<T extends Place> extends SignableDao<T> {
 }
