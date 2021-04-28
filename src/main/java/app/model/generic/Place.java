f38fc705177bc7b44bee9b8e3eea6722c2ef11b0
Miguel Ángel Prosper <37023010+MAProsper@users.noreply.github.com>
2021-04-28 20:52:45 +0200

    stuff

--- src/main/java/app/model/generic/Place.java
+++ src/main/java/app/model/generic/Place.java
@@ -3,6 +3,14 @@ package app.model.generic;
 public abstract class Place extends Signable {
     public String name;
 
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
     @Override
     public String toString() {
         return "Place{" +
