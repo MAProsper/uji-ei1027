abd31fb7e21b4a1e969f39a31d725e84e0047600
Paul <al386152@uji.es>
2021-05-11 09:40:32 +0200

    SessionableController.v2

--- src/main/java/app/controller/ReservationController.java
+++ src/main/java/app/controller/ReservationController.java
@@ -3,6 +3,7 @@ package app.controller;
 import app.controller.generic.Controller;
 import app.controller.generic.SessionableController;
 import app.model.Reservation;
+import app.model.generic.Person;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @org.springframework.stereotype.Controller
--- src/main/java/app/controller/generic/SessionableController.java
+++ src/main/java/app/controller/generic/SessionableController.java
@@ -1,6 +1,8 @@
 package app.controller.generic;
 
 import app.model.generic.Model;
+import app.model.generic.Person;
+
 import javax.servlet.http.HttpSession;
 
 
@@ -9,8 +11,12 @@ import javax.servlet.http.HttpSession;
  */
 public abstract class SessionableController<M extends Model> extends Controller<M>{
 
+    public boolean autentificate(Person person){
+        return person != null;
+    }
+
     @Override
     public String list(org.springframework.ui.Model model, HttpSession session) {
-        return session.getAttribute("user") == null ?  "redirect:/session/add" : super.list(model, session);
+        return this.autentificate((Person) session.getAttribute("user")) ? super.list(model, session) : "redirect:/session/add";
     }
 }
