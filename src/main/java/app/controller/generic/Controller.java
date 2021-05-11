cf458d94a765355c5c663a185f495a87e2756fc2
Miguel Ángel Prosper <37023010+MAProsper@users.noreply.github.com>
2021-05-11 09:40:30 +0200

    stuff

--- src/main/java/app/controller/generic/Controller.java
+++ src/main/java/app/controller/generic/Controller.java
@@ -20,7 +20,7 @@ public abstract class Controller<M extends Model> {
     @Autowired protected Validator<M> validator;
 
     @RequestMapping("/list")
-    public String list(org.springframework.ui.Model model, HttpSession session) {
+    public String list(HttpSession session, org.springframework.ui.Model model) {
         List<M> objects = service.getAll(session);
         model.addAttribute("objects", objects);
         model.addAttribute("data", objects.stream().map(service::otherData).collect(Collectors.toList()));
@@ -28,29 +28,29 @@ public abstract class Controller<M extends Model> {
     }
 
     @RequestMapping("/add")
-    public String add(org.springframework.ui.Model model, HttpSession session) {
+    public String add(HttpSession session, org.springframework.ui.Model model) {
         model.addAttribute("object", getDao().getReflect().newInstance());
         return getView("add");
     }
 
     @RequestMapping(path = "/add", method = RequestMethod.POST)
-    public String add(@ModelAttribute M object, BindingResult binding, HttpSession session) {
+    public String add(HttpSession session, @ModelAttribute M object, BindingResult binding) {
         return executePost("add", object, binding);
     }
 
     @RequestMapping("/update/{id}")
-    public String update(org.springframework.ui.Model model, @PathVariable int id, HttpSession session) {
+    public String update(HttpSession session, org.springframework.ui.Model model, @PathVariable int id) {
         model.addAttribute("object", getDao().getById(id));
         return getView("update");
     }
 
     @RequestMapping(path = "/update/{id}", method = RequestMethod.POST)
-    public String update(@ModelAttribute M object, BindingResult binding, HttpSession session) {
+    public String update(HttpSession session, @ModelAttribute M object, BindingResult binding) {
         return executePost("update", object, binding);
     }
 
     @RequestMapping("/delete/{id}")
-    public String delete(org.springframework.ui.Model model, @PathVariable int id, HttpSession session) {
+    public String delete(HttpSession session, org.springframework.ui.Model model, @PathVariable int id) {
         getDao().delete(id);
         return "redirect:../list";
     }
