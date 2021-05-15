4b1ba3d4431b52da2255c2ccd87bd7653adf7f5b
Miguel Ángel Prosper <37023010+MAProsper@users.noreply.github.com>
2021-05-15 14:10:28 +0200

    stuff

--- src/main/java/app/validator/ZoneValidator.java
+++ src/main/java/app/validator/ZoneValidator.java
@@ -1,16 +1,21 @@
 package app.validator;
 
+import app.dao.AreaDao;
 import app.model.MunicipalManager;
 import app.model.Zone;
 import app.validator.generic.PlaceValidator;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import javax.servlet.http.HttpSession;
 
 @Service
 public class ZoneValidator extends PlaceValidator<Zone> {
+    @Autowired AreaDao areaDao;
+
     @Override
     public boolean list(HttpSession session, int arg) {
+        if (areaDao.getById(arg) == null) return forbidden();
         return ifPerson(session, MunicipalManager.class);
     }
 }
