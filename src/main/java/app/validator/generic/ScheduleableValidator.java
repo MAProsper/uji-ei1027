5f55a61331a3a73d6498333a3e586a0096507a49
Paul <al386152@uji.es>
2021-05-23 16:15:38 +0200

    ScheduleableValidator modificado

--- src/main/java/app/validator/generic/ScheduleableValidator.java
+++ src/main/java/app/validator/generic/ScheduleableValidator.java
@@ -7,10 +7,16 @@ public abstract class ScheduleableValidator<T extends Scheduleable> extends Vali
     public void object(T object, FieldErrors errors) {
         super.object(object, errors);
 
-        if (object.getScheduleStart().isBefore(object.getScheduleEnd()))
-            errors.accept("scheduleEnd", "La fecha de fin deber ser posterior a la de incio");
+        if (object.getScheduleStart() == null)
+            errors.accept("scheduleStart", "Formato de fecha de inicio inválida");
+        else if (object.getScheduleEnd() != null && object.getScheduleStart().isBefore(object.getScheduleEnd()))
+            errors.accept("scheduleEnd", "La fecha de fin deber ser posterior o igual a la de inicio");
 
-        if (object.getPeriodStart().compareTo(object.getPeriodEnd()) >= 0)
-            errors.accept("periodEnd", "La hora de fin deber ser posterior a la de incio");
+        if (object.getPeriodStart() == null)
+            errors.accept("periodStart", "Formato de hora de inicio inválida");
+        else if (object.getPeriodEnd() == null)
+            errors.accept("periodEnd", "Formato de hora de fin inválida");
+        else if (object.getPeriodStart().compareTo(object.getPeriodEnd()) >= 0)
+            errors.accept("periodEnd", "La hora de fin deber ser posterior a la de inicio");
     }
 }
