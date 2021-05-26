462e58f22e199b1e691553342c240dcc2faef1c2
Miguel Ángel Prosper <37023010+MAProsper@users.noreply.github.com>
2021-05-26 23:01:33 +0200

    stuff

--- src/main/java/app/controller/ReservationController.java
+++ src/main/java/app/controller/ReservationController.java
@@ -1,10 +1,29 @@
 package app.controller;
 
+import app.ApplicationException;
 import app.model.Reservation;
+import net.glxn.qrgen.javase.QRCode;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.net.InetAddress;
 
 @Controller
 @RequestMapping("/reservation")
 public class ReservationController extends app.controller.generic.Controller<Reservation> {
+    @RequestMapping(path = "/image/{arg}")
+    public String list(@PathVariable Integer arg, HttpServletResponse response, @Value("${local.server.port}") int port) {
+        try {
+            String host = InetAddress.getLocalHost().getHostAddress();
+            String url = String.format("http://%s:%d/reservation/update/%d", host, port, arg);
+            QRCode.from(url).writeTo(response.getOutputStream());
+        } catch (IOException e) {
+            throw new ApplicationException("Error generando la imagen", e);
+        }
+        return null;
+    }
 }
