991680967f13d032d618bc6ccbd29b0a5ad89b7f
Miguel Ángel Prosper <37023010+MAProsper@users.noreply.github.com>
2021-06-14 23:41:59 +0200

    final final

--- src/main/java/app/service/MunicipalManagerService.java
+++ src/main/java/app/service/MunicipalManagerService.java
@@ -1,23 +1,41 @@
 package app.service;
 
+import app.dao.MunicipalManagerDao;
+import app.dao.MunicipalityDao;
 import app.model.MunicipalManager;
 import app.service.generic.PersonService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import javax.servlet.http.HttpSession;
 import java.util.Collections;
 import java.util.List;
+import java.util.Map;
 
 @Service
 public class MunicipalManagerService extends PersonService<MunicipalManager> {
+    @Autowired MunicipalityDao municipalityDao;
+
     @Override
     public List<MunicipalManager> listObjects(HttpSession session, Integer arg) {
         return Collections.singletonList((MunicipalManager) getUser(session));
     }
 
     @Override
-    public MunicipalManager updateObject(HttpSession session, Integer arg) {
+    public Map<String, Object> data(MunicipalManager object) {
+        Map<String, Object> data = super.data(object);
+        data.put("municipality", municipalityDao.getParentOf(object));
+        return data;
+    }
+
+    @Override
+    public MunicipalManager addObject(HttpSession session, Integer arg) {
         return (MunicipalManager) getUser(session);
+    }
+
+    @Override
+    public MunicipalManager updateObject(HttpSession session, Integer arg) {
+        return addObject(session, arg);
     }
 
     @Override
